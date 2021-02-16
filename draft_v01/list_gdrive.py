f32772b4919a5446b41b4d1a6b6e5ed22bb94dd2
Tony Held <tony.held@gmail.com>
2021-02-16 13:37:13 -0800

    Follow up of initial commit

---
+++ draft_v01/list_gdrive.py
@@ -0,0 +1,18 @@
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+CLIENT_SECRET = 'client_secret.json'
+
+store = file.Storage('storage.json')
+credz = store.get()
+if not credz or credz.invalid:
+    flow = client.flow_from_clientsecrets(CLIENT_SECRET, SCOPES)
+    credz = tools.run_flow(flow, store)
+
+SERVICE = build('drive', 'v2', http=credz.authorize(Http()))
+files = SERVICE.files().list().execute().get('items', [])
+
+for f in files:
+    print(f['title'], f['mimeType'])
